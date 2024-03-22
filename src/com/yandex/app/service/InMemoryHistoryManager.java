93e05a003d93f921f0aab7db3d0f055c8eb3875e
AveryanovaKatia <160598695+AveryanovaKatia@users.noreply.github.com>
2024-03-22 16:02:51 +0400

    трекер задач. ТЗ 5 - третья версия

--- src/com/yandex/app/service/InMemoryHistoryManager.java
+++ src/com/yandex/app/service/InMemoryHistoryManager.java
@@ -7,22 +7,22 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private final List<Task> historyManager = new LinkedList<>();
+    private final List<Task> historyList= new LinkedList<>();
     private final static int SIZE = 10;
 
     @Override
     public void addTaskInHistory(Task task) {
         if (task != null) {
-            if (historyManager.size() == SIZE) {
-                historyManager.remove(0);
+            if (historyList.size() == SIZE) {
+                historyList.remove(0);
             }
-            historyManager.add(task);
+            historyList.add(task);
         }
     }
 
     @Override
     public List<Task> getHistory() {
-        return List.copyOf(historyManager);
+        return List.copyOf(historyList);
     }
 
 
