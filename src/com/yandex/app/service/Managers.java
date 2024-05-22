180453e32adca00bd8dcd71ceaa455cbca5b843a
AveryanovaKatia <160598695+AveryanovaKatia@users.noreply.github.com>
2024-05-23 00:21:49 +0400

    Реализовать ТЗ-9

--- src/com/yandex/app/service/Managers.java
+++ src/com/yandex/app/service/Managers.java
@@ -1,11 +1,7 @@
 package com.yandex.app.service;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.yandex.app.service.inmemory.InMemoryHistoryManager;
 import com.yandex.app.service.inmemory.InMemoryTaskManager;
-
-import java.time.LocalDateTime;
 
 public class Managers {
     private Managers() {
@@ -18,10 +14,4 @@ public class Managers {
     public static HistoryManager getDefaultHistory() {
         return new InMemoryHistoryManager();
     }
-
-//    public static Gson getGson() {
-//        GsonBuilder gsonBuilder = new GsonBuilder();
-//        gsonBuilder.registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter());
-//        return gsonBuilder.create();
-//    }
 }
